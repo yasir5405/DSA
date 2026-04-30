aa9155c45ef6d940cf540af7b331c6e4e24240b5
yasir5405 <yasirnaseem5415@gmail.com>
2026-04-30 17:16:27 +0530

    Linked list demo

---
+++ Linked List/demo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    Node n1(1123);
+
+    cout << n1.data;
+}
