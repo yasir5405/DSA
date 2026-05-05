ad7b55603e74ff63194499cd24c6d8a33a1739e3
yasir5405 <yasirnaseem5415@gmail.com>
2026-05-05 18:04:47 +0530

    Double linked list demo

---
+++ Linked List/DoublyLinked.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    Node(int d)
+    {
+        this->data = d;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void print(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *n = new Node(69);
+
+    head->next = n;
+    n->prev = head;
+
+    print(head);
+}
