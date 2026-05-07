8a615435ee21e9dfb01aca7666e0695bae88b796
yasir5405 <yasirnaseem5415@gmail.com>
2026-05-07 18:29:15 +0530

    Deletion in a singly linked circular list

--- Linked List/circular.cpp
+++ Linked List/circular.cpp
@@ -42,9 +42,69 @@ void insertNode(Node *&tail, int elem, int data)
     }
 }
 
+void deleteNode(Node *&tail, int value)
+{
+    // first elem
+    if (tail == NULL)
+    {
+        return;
+    }
+    else
+    {
+        Node *prev = tail;
+        Node *curr = prev->next;
+
+        while (curr->data != value)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+
+        prev->next = curr->next;
+        // single node list
+        if (curr == prev)
+        {
+            tail = NULL;
+        }
+
+        // deleting tail
+        else if (tail == curr)
+        {
+            tail = prev;
+        }
+
+        curr->next = NULL;
+        delete curr;
+    }
+}
+
+void print(Node *tail)
+{
+    if (tail == NULL)
+    {
+        cout << "List is empty." << endl;
+        return;
+    }
+
+    Node *temp = tail;
+
+    do
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    } while (temp != tail);
+
+    cout << endl;
+}
 int main()
 {
     Node *tail = NULL;
 
     insertNode(tail, 5, 1);
+    insertNode(tail, 1, 2);
+    insertNode(tail, 2, 3);
+    print(tail);
+
+    deleteNode(tail, 1);
+    print(tail);
 }
